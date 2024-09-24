fb7711855c3d88976edae5956925275ddfe504c2
Vitaly Trakhtenberg <webdev@tca.by>
2024-09-24 21:06:01 +0300

    created iterator class CompanyIterator

--- src/main/java/telran/employees/CompanyImpl.java
+++ src/main/java/telran/employees/CompanyImpl.java
@@ -2,7 +2,8 @@ package telran.employees;
 
 import java.util.*;
 
-public class CompanyImpl implements Company{
+public class CompanyImpl implements Company
+{
     private TreeMap<Long, Employee> employees = new TreeMap<>();
     private HashMap<String, List<Employee>> employeesDepartment = new HashMap<>();
     private TreeMap<Float, List<Manager>> managersFactor = new TreeMap<>();
@@ -10,7 +11,7 @@ public class CompanyImpl implements Company{
     @Override
     public Iterator<Employee> iterator() 
     {
-       return new CompanyIterator();    //TODO
+       return new CompanyIterator();
     }
 
     @Override
@@ -58,6 +59,90 @@ public class CompanyImpl implements Company{
     {
         // TODO Implement this method
         throw new UnsupportedOperationException("Method CompanyImpl.getManagersWithMostFactor() not implemented yet");
+    }
+
+    private class CompanyIterator implements Iterator<Employee>
+    {
+        private final Iterator<Employee> iterator = employees.values().iterator();
+        private Employee last_iterated;
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public Employee next() 
+        {
+            last_iterated = iterator.next();
+            return last_iterated;
+        }
+
+        /**
+         * Removes from the underlying collection the last element returned
+         * by this iterator (optional operation).  This method can be called
+         * only once per call to {@link #next}.
+         * <p>
+         * The behavior of an iterator is unspecified if the underlying collection
+         * is modified while the iteration is in progress in any way other than by
+         * calling this method, unless an overriding class has specified a
+         * concurrent modification policy.
+         * <p>
+         * The behavior of an iterator is unspecified if this method is called
+         * after a call to the {@link #forEachRemaining forEachRemaining} method.
+         *
+         * @throws UnsupportedOperationException if the {@code remove}
+         *                                       operation is not supported by this iterator
+         * @throws IllegalStateException         if the {@code next} method has not
+         *                                       yet been called, or the {@code remove} method has already
+         *                                       been called after the last call to the {@code next}
+         *                                       method
+         * @implSpec The default implementation throws an instance of
+         * {@link UnsupportedOperationException} and performs no other action.
+         */
+        @Override
+        public void remove() {
+            iterator.remove();
+            if (last_iterated != null) {
+                removeEmployeeFromIndexMaps(last_iterated);
+                last_iterated = null;
+            }
+        }
+
+        private void removeEmployeeFromIndexMaps(Employee last_iterated_employee)
+        {
+            List<Employee> departmentEmployees = employeesDepartment.get(last_iterated_employee.getDepartment());
+            if (departmentEmployees != null) {
+                departmentEmployees.remove(last_iterated_employee);
+                if (departmentEmployees.isEmpty()) {
+                    employeesDepartment.remove(last_iterated_employee.getDepartment());
+                }
+            }
+
+            if (last_iterated_employee instanceof Manager) {
+                Manager manager = (Manager) last_iterated_employee;
+                List<Manager> factorManagers = managersFactor.get(manager.getFactor());
+                if (factorManagers != null) {
+                    factorManagers.remove(manager);
+                    if (factorManagers.isEmpty()) {
+                        managersFactor.remove(manager.getFactor());
+                    }
+                }
+            }
+        }
     }
 
 }
